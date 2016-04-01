50b0c9103dac985a10cc4aba14bc490b0f2dd77e
ivan.petrov <ivan.petrov@Ivan-E540.musala.com>
2016-04-01 18:25:15 +0300

    Made intersection and union (problem6)

--- src/problem6/IntersectionUnion.java
+++ src/problem6/IntersectionUnion.java
@@ -1,6 +1,9 @@
 package problem6;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class IntersectionUnion {
 
@@ -14,23 +17,84 @@ public class IntersectionUnion {
 	// + и * означаваме съответно обединение и сечение на множества.
 
 	public static void main(String[] args) {
-		System.out.println(multipleInts());
+		System.out.println(f1());
+		System.out.println(f2());
+		System.out.println(f3());
+		Set<Integer> f1Set = new HashSet<>(f1());
+		Set<Integer> f2Set = new HashSet<>(f2());
+		Set<Integer> f3Set = new HashSet<>(f3());
+		System.out.println(unionTwoSets(f1Set, f2Set));
+		System.out.println(unionTwoSets(f1Set, f3Set));
+		System.out.println(unionTwoSets(f2Set, f3Set));
+		System.out.println(unionThreeSets(f1Set, f2Set, f3Set));
+		System.out.println(intersectTwoSets(f1Set, f2Set));
+		System.out.println(intersectTwoSets(f1Set, f3Set));
+		System.out.println(intersectTwoSets(f2Set, f3Set));
+		System.out.println(intersectThreeSets(f1Set, f2Set, f3Set));
 
 	}
 
-	private static ArrayList<Integer> multipleInts() {
+	private static ArrayList<Integer> f1() {
 		int element;
-		ArrayList<Integer> arrayList = new ArrayList<Integer>();
-		for (int i = 0; i <= 1000; i++) {
-			element = addElement(i);
+		ArrayList<Integer> arrayList = new ArrayList<>();
+		arrayList.add(1);
+		for (int i = 1; i <= 15; i++) {
+			element = 2 * arrayList.get(i - 1) + 3;
 			arrayList.add(element);
 		}
 		return arrayList;
 	}
 
-	private static int addElement(int i) {
-		int number = 2 * i + 3;
-		return number;
+	private static ArrayList<Integer> f2() {
+		int element;
+		ArrayList<Integer> arrayList = new ArrayList<>();
+		arrayList.add(2);
+		for (int i = 1; i <= 15; i++) {
+			element = 3 * arrayList.get(i - 1) + 1;
+			arrayList.add(element);
+
+		}
+		return arrayList;
 	}
 
+	private static ArrayList<Integer> f3() {
+		int element;
+		ArrayList<Integer> arrayList = new ArrayList<>();
+		arrayList.add(2);
+		for (int i = 1; i <= 15; i++) {
+			element = 2 * arrayList.get(i - 1) - 1;
+			arrayList.add(element);
+		}
+		return arrayList;
+	}
+
+	private static <E> Set<E> unionTwoSets(Set<E> set1, Set<E> set2) {
+		Set<E> union = new HashSet<E>();
+		union.addAll(set1);
+		union.addAll(set2);
+		return new TreeSet<E>(union);
+	}
+
+	private static <E> Set<E> unionThreeSets(Set<E> set1, Set<E> set2, Set<E> set3) {
+		Set<E> union = new HashSet<E>();
+		union.addAll(set1);
+		union.addAll(set2);
+		union.addAll(set3);
+		return new TreeSet<E>(union);
+	}
+
+	private static <E> Set<E> intersectTwoSets(Set<E> set1, Set<E> set2) {
+		Set<E> intersect = new HashSet<E>();
+		intersect.addAll(set1);
+		intersect.retainAll(set2);
+		return new TreeSet<E>(intersect);
+	}
+
+	private static <E> Set<E> intersectThreeSets(Set<E> set1, Set<E> set2, Set<E> set3) {
+		Set<E> intersect = new HashSet<E>();
+		intersect.addAll(set1);
+		intersect.retainAll(set2);
+		intersect.retainAll(set3);
+		return new TreeSet<E>(intersect);
+	}
 }
